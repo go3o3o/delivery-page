2121419ec16901533c0bb726e3ece577ad9fcd26
yonikim <yonikim@wisenut.co.kr>
2020-06-25 11:02:01 +0900

    clean push

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import autobind from 'autobind-decorator';
+import { Layout } from 'antd';
 
 import 'antd/dist/antd.css';
+import './css/style.css';
 
 import { PAGE_PATHS, STORES } from './constants';
 import Login from './pages/Signin';
@@ -17,16 +19,18 @@ import 'antd/dist/antd.css';
 export default class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path={PAGE_PATHS.CATEGORY_LISTS} component={CategoryList} />
-          <Route path={PAGE_PATHS.SIGNIN} component={Login} />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route path={PAGE_PATHS.CATEGORY_LISTS} component={CategoryList} />
+            <Route path={PAGE_PATHS.SIGNIN} component={Login} />
 
-          <Route path={`${PAGE_PATHS.CATEGORY_LISTS}`} component={CategoryList} />
+            <Route path={`${PAGE_PATHS.CATEGORY_LISTS}`} component={CategoryList} />
 
-          <Redirect from="/" to={PAGE_PATHS.CATEGORY_LISTS} />
-        </Switch>
-      </Router>
+            <Redirect from="/" to={PAGE_PATHS.CATEGORY_LISTS} />
+          </Switch>
+        </Router>
+      </Layout>
     );
   }
 }
--- client/src/pages/Signin/index.tsx
+++ client/src/pages/Signin/index.tsx
@@ -2,9 +2,14 @@ import React, { useEffect, ChangeEvent } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { inject, observer } from 'mobx-react';
 
+import { Layout, Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined, TranslationOutlined } from '@ant-design/icons';
+
 import { PAGE_PATHS, STORES } from '../../constants';
 import AuthStore from '../../stores/auth/AuthStore';
 import Header from '../../components/Header';
+
+const { Content } = Layout;
 
 interface InjectedProps {
   [STORES.AUTH_STORE]: AuthStore;
@@ -32,12 +37,59 @@ function Signin(props: InjectedProps & RouteComponentProps) {
     authStore.setEmail(v.target.value);
   };
 
+  const style = {
+    width: '50%',
+  };
+
   return (
     <>
-      <div style={{ width: '100%', height: '100%', backgroundColor: '#5FBEBB' }}>
-        <Header />
-        <div>Login</div>
-      </div>
+      <Content
+        style={{
+          backgroundColor: '#5FBEBB',
+          height: '100vh',
+          position: 'relative',
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            backgroundColor: '#FFF',
+            height: '400px',
+            width: '30%',
+            top: '50%',
+            left: '50%',
+            transform: `translate(-50%, -50%)`,
+          }}
+        >
+          <Form.Item style={style}>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <Form.Item>
+            <Button
+              style={{
+                backgroundColor: '#5FBEBB',
+                borderColor: 'white',
+                height: 40,
+              }}
+            >
+              <span style={{ color: 'white', fontSize: 20 }}> Log in</span>
+            </Button>
+          </Form.Item>
+        </div>
+      </Content>
     </>
   );
 }

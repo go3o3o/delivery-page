f4779c2398e7c229cd7be1dafbcd9b1ee6280710
yonikim <yonikim@wisenut.co.kr>
2020-07-13 17:38:31 +0900

    category request method 변경

--- server/src/routes/category.ts
+++ server/src/routes/category.ts
@@ -7,14 +7,12 @@ import { Category } from '../entities/Category';
 
 const router = express.Router();
 
-router.get('', async (req, res) => {
+router.post('', async (req, res) => {
   const manager = getConnectionManager().get('delivery');
   const repository = manager.getRepository(Category).createQueryBuilder();
 
   try {
     const category = await repository.select().getMany();
-
-    console.log(category);
 
     return res.json({ data: category });
   } catch (e) {
@@ -22,8 +20,7 @@ router.get('', async (req, res) => {
   }
 });
 
-// 0: 한식, 1: 중식, 2: 분식, 3: 치킨, 4: 피자, 5: 패스트푸드, 6: 일식, 7: 양식, 8: 보쌈|족발, 9: 야식
-router.post('/:category', async (req, res) => {
+router.get('/:category', async (req, res) => {
   const category = req.params.category;
 
   const manager = getConnectionManager().get('delivery');

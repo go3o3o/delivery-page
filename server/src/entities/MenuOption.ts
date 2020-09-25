dc3d614a27af265c2cb2775e011de8ce67cfc28c
yonikim <yonikim@wisenut.co.kr>
2020-09-25 15:40:26 +0900

    DB table 구성 수정

--- server/src/entities/MenuOption.ts
+++ server/src/entities/MenuOption.ts
@@ -1,24 +1,21 @@
 import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm';
 
-@Entity({ name: 'tb_menu' })
+@Entity({ name: 'tb_menu_option' })
 export class Menu extends BaseEntity {
   @PrimaryGeneratedColumn()
   public seq!: number;
 
   @Column()
-  public store_seq!: number;
+  public menu_seq!: number;
 
   @Column()
-  public menu_name!: string;
+  public menu_option_grp!: string;
 
   @Column()
-  public menu_img!: string;
+  public menu_option!: string;
 
   @Column()
-  public menu_grp!: string;
-
-  @Column()
-  public menu_desc!: string;
+  public menu_price!: number;
 
   public readonly reg_dt!: Date;
   public readonly upd_dt!: Date;
